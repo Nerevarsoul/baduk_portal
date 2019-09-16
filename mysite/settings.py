692656eb89efa26c5cd946b2feecc6c43cdb4182
neri <neri@gudzumi.ru>
2019-09-16 22:15:40 +0300

    add allowed hosts

--- mysite/settings.py
+++ mysite/settings.py
@@ -25,7 +25,7 @@ SECRET_KEY = '208y10$7_qn0-5vk26v#!bc^zo9fn89l=k_@9j3#fs_axtx@e+'
 # SECURITY WARNING: don't run with debug turned on in production!
 DEBUG = True
 
-ALLOWED_HOSTS = []
+ALLOWED_HOSTS = ['212.8.247.61']
 
 
 # Application definition
