145001380a696fcd3fa4aaa44ae6819e8dbe82e7
Innokentiy <keshaeverywhere@gmail.com>
2019-09-16 22:11:36 +0300

    Static from gunicorn in debug mode

--- mysite/urls.py
+++ mysite/urls.py
@@ -1,4 +1,6 @@
+from django.conf import settings
 from django.contrib import admin
+from django.contrib.staticfiles.urls import staticfiles_urlpatterns
 from django.urls import path, include
 
 urlpatterns = [
@@ -6,3 +8,6 @@ urlpatterns = [
     path('tsumego/', include('tsumego.urls')),
     path('admin/dynamic_raw_id/', include('dynamic_raw_id.urls')),
 ]
+
+if settings.DEBUG:
+    urlpatterns += staticfiles_urlpatterns()
