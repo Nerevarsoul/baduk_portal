7c15a6b53583455a5ba39c41092916502b9e1740
Nerevarsoul <keshaeverywhere@gmail.com>
2019-11-06 22:09:20 +0300

    fix static

--- mysite/urls.py
+++ mysite/urls.py
@@ -2,6 +2,7 @@ from django.conf import settings
 from django.contrib import admin
 from django.contrib.staticfiles.urls import staticfiles_urlpatterns
 from django.urls import path, include
+from django.conf.urls.static import static
 
 urlpatterns = [
     path('admin/', admin.site.urls),
@@ -10,5 +11,6 @@ urlpatterns = [
     path('admin/dynamic_raw_id/', include('dynamic_raw_id.urls')),
 ]
 
-if settings.DEBUG:
+if True:
     urlpatterns += staticfiles_urlpatterns()
+    urlpatterns += static(settings.STATIC_URL, document_root=settings.STATIC_ROOT)
