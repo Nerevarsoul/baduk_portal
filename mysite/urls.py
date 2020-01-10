9b975a334a7eef1245a4b7e0d18a60e0ec0e018b
Innokentiy <keshaeverywhere@gmail.com>
2020-01-10 15:16:27 +0700

    create base template and homepage

--- mysite/urls.py
+++ mysite/urls.py
@@ -1,10 +1,13 @@
 from django.conf import settings
+from django.conf.urls.static import static
 from django.contrib import admin
 from django.contrib.staticfiles.urls import staticfiles_urlpatterns
+from django.views.generic import TemplateView
 from django.urls import path, include
-from django.conf.urls.static import static
+
 
 urlpatterns = [
+    path('', TemplateView.as_view(template_name="homepage.html")),
     path('admin/', admin.site.urls),
     path('tsumego/', include('tsumego.urls')),
     path('tournament/', include('tournament.urls')),
