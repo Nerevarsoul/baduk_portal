82fb163beca588c6f62417887f39786e9b3036b4
Innokentiy <keshaeverywhere@gmail.com>
2020-02-13 11:48:18 +0700

    show only active tournament in last view

--- tournament/managers.py
+++ tournament/managers.py
@@ -4,10 +4,12 @@ from django.db.models import QuerySet, Manager
 class TournamentQuerySet(QuerySet):
 
     def details(self, id):
-        return self.filter(id=id).prefetch_related('participants', 'games', 'participants__user').first()
+        return self.filter(id=id).prefetch_related(
+            'participants', 'games', 'participants__user'
+        ).first()
 
     def last_by_title(self, title_id):
-        return self.filter(title=title_id).prefetch_related(
+        return self.filter(title=title_id, is_active=True).prefetch_related(
             'participants', 'games', 'participants__user'
         ).order_by('-id').first()
 
